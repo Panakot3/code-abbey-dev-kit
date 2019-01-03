c32a0cf417b92520c3cb7c0906f7da161e85e173
Ruslan <afro.funky.lover@gmail.com>
2019-01-03 15:29:41 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+code-abbey-dev-kit
---
+++ app.js
@@ -0,0 +1,16 @@
+let _c = 0;
+function input() {
+    const line = data.split()[_c];
+    _c++;
+    return line;
+}
+
+function output(a) {
+    console.log(a);
+}
+
+// test data
+let data = 
+``;
+
+// your code start
